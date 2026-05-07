0373d6007f076d31a3bd4587cb3bdc84340c0a45
erikmorenopino22-dot <erikmorenopino22@gmail.com>
2026-05-07 19:18:36 +0200

    actual

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
         System.out.println("Hola Mundo");
         System.out.println("Adios Mundo");
         System.out.println("Bienvenido a Java");
+        System.out.println("Aprendiendo a programar en Java");
         // Agrega más mensajes aquí
     }
 }
